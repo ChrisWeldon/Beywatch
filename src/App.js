03f6a384abbd8b53cddfa8eb5174228af5cd03ee
Chris Laptop <cwevans612@gmail.com>
2019-05-03 17:01:38 -0400

    ejecting

--- src/App.js
+++ src/App.js
@@ -5,20 +5,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid ">
+        <nav class="navbar navbar-light bg-light row justify-content-center">
+          <a class="navbar-brand" href="#">
+            Beyblade Matchup Ranking
+          </a>
+        </nav>
+        <div className="row">
+          <div className="col-md-6">
+
+          <form>
+            <div class="form-row align-items-center">
+              <div class="col-auto my-1">
+                <label class="mr-sm-2" for="inlineFormCustomSelect">Preference</label>
+                <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                  <option selected>Choose...</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+              </div>
+              <div class="col-auto my-1">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </div>
+            </form>
+
+          </div>
+          <div className="col-md-6">
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
