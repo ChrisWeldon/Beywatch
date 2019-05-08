69825831aa7d436ff211c0184f51acce03306223
Chris Laptop <cwevans612@gmail.com>
2019-05-08 17:29:40 -0400

    Filter Bar

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-                  apiResponse: "",
                   el1:{
                     img_url : ""
                   },
@@ -24,10 +23,17 @@ class App extends Component {
                   },
                   pt2:{
                     img_url : ""
-                  }
+                  },
+                  el1Results:[],
+                  el2Results:[],
+                  fd1Results:[],
+                  fd2Results:[],
+                  pt1Results:[],
+                  pt2Results:[]
                 };
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
+    this.handleOptionClick = this.handleOptionClick.bind(this)
   }
 
   componentDidMount(){
@@ -85,7 +91,6 @@ class App extends Component {
       body: JSON.stringify({part_type:type, query:val})
     }).then(res => res.json())
     .then(res => {
-      console.log(res)
       if(res.length ==0 ){
         console.log("sql query empty")
         let obj = {};
@@ -95,10 +100,48 @@ class App extends Component {
         let obj = {};
         obj[id] = val;
         obj[name] = res[0]
+        obj[name+'Results'] = res
         this.setState(obj)
       }
     });
   }
+
+  handleOptionClick(event){
+    let obj = {}
+    let val = event.target.textContent;
+    let type;
+    let name = event.target.name;
+
+    if(name.includes("el")){
+      type = "Energy Layer";
+    } else if(name.includes("fd")){
+      type = "Forge Disc"
+    } else{
+      type="Performance Tip"
+    }
+
+    let id = event.target.id
+    fetch('http://localhost:9000/api/part', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({part_type:type, query:val})
+    }).then(res => res.json())
+    .then(res => {
+      if(res.length ==0 ){
+        console.log("sql query empty")
+        let obj = {};
+        obj[id] = val;
+        this.setState(obj);
+      }else{
+        let obj = {};
+        obj[id] = val;
+        obj[name] = res[0]
+        obj[name+'Results'] = res
+        this.setState(obj)
+      }
+    });
+  }
+
 
 
   renderPart(pt, q, part){
@@ -114,6 +157,17 @@ class App extends Component {
     });
   }
 
+  returnQueriedMenuList(part){
+    console.log(this.state[part+"Results"])
+    let ret_array = []
+    for(let i=0; i<this.state[part+"Results"].length;i++){
+      ret_array.push(<span><a id="el1Input" name="el1" className="btn" onClick={this.handleOptionClick}>{this.state[part+"Results"][i].name_hasbro}</a><br/></span>)
+    }
+    return(
+      ret_array
+    )
+  }
+
   render(){
     console.log(this.state)
     return (
@@ -127,8 +181,13 @@ class App extends Component {
               <div className="col-md-6">
                   <h3>Beyblade 1</h3>
                   <div className="form-group align-items-center form-row">
-                    <div className="col-8">
+                    <div className="col-sm-4">
                       <input value={this.state.el1Input} onChange={this.handleChange} id="el1Input" class="form-control" aria-describedby="el1" placeholder="Energy Layer Name" name="el1"/>
+                    </div>
+                    <div className="col-4">
+                      <div className="scrollable-menu">
+                        {this.returnQueriedMenuList("el1")}
+                      </div>
                     </div>
                     <div className="col-4">
                       <img src={this.state.el1.img_url} className="partShow" alt="el1" />
@@ -150,6 +209,12 @@ class App extends Component {
                       <img src={this.state.pt1.img_url} className="partShow" alt="pt1" />
                     </div>
                   </div>
+                  <p>Attack: {this.state.el1.attack + this.state.fd1.attack + this.state.pt1.attack}&nbsp;
+                  Defense: {this.state.el1.defense + this.state.fd1.defense + this.state.pt1.defense}&nbsp;
+                  Stamina: {this.state.el1.stamina + this.state.fd1.stamina + this.state.pt1.stamina}&nbsp;
+                  Weight: {this.state.el1.weight_stat + this.state.fd1.weight_stat + this.state.pt1.weight_stat}&nbsp;
+                  Speed: {this.state.el1.speed + this.state.fd1.speed + this.state.pt1.speed}&nbsp;
+                  Burst: {this.state.el1.burst}</p>
                 </div>
                 <div className="col-md-6">
                     <h3>Beyblade 2</h3>
@@ -177,6 +242,12 @@ class App extends Component {
                         <input value={this.state.pt2Input} onChange={this.handleChange} id="pt2Input" class="form-control" aria-describedby="pt2" placeholder="Forge Disc Name" name="pt2"/>
                       </div>
                     </div>
+                    <p>Attack: {this.state.el2.attack + this.state.fd2.attack + this.state.pt2.attack}&nbsp;
+                    Defense: {this.state.el2.defense + this.state.fd2.defense + this.state.pt2.defense}&nbsp;
+                    Stamina: {this.state.el2.stamina + this.state.fd2.stamina + this.state.pt2.stamina}&nbsp;
+                    Weight: {this.state.el2.weight_stat + this.state.fd2.weight_stat + this.state.pt2.weight_stat}&nbsp;
+                    Speed: {this.state.el2.speed + this.state.fd2.speed + this.state.pt2.speed}&nbsp;
+                    Burst: {this.state.el2.burst}</p>
                   </div>
                 </div>
               <div className="row justify-content-center">
